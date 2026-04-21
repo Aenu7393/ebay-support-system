176471186f5b48e95073624988be654b3a9f059f
natuki <namikin536373@gmail.com>
2026-04-21 22:50:35 +0900

    Add collectstatic for Docker deploy

--- project_web/workweb/settings.py
+++ project_web/workweb/settings.py
@@ -66,7 +66,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', #追加
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
